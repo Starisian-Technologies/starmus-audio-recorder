7bd33d2ddd65fe95b133a508088374f566e5a03d
Max Barrett <maximilliangroup@gmail.com>
2025-05-20 18:19:31 -0700

    05202025MB6

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'selector-class-pattern': null,
+    'selector-id-pattern': null,
+    'string-quotes': 'single',
+    'block-no-empty': true,
+    'color-no-invalid-hex': true
+  }
+};
