04abb8889af4f25a7b2e321f3f962108f8e9222b
Max Barrett <maximilliangroup@gmail.com>
2025-08-24 17:56:17 -0700

    Update stylelint.config.js
    Signed-off-by: Max Barrett <maximilliangroup@gmail.com>

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,13 +1,12 @@
+// stylelint.config.cjs
 module.exports = {
-  extends: [
-    "stylelint-config-standard"
-  ],
+  extends: ['stylelint-config-standard'],
   rules: {
-    "indentation": 2,
-    "string-quotes": "double",
-    "color-hex-case": "lower",
-    "block-no-empty": true,
-    "no-empty-source": true,
-    "declaration-block-trailing-semicolon": "always"
+    indentation: 2,
+    'string-quotes': 'double',
+    'color-hex-case': 'lower',
+    'block-no-empty': true,
+    'no-empty-source': true,
+    'declaration-block-trailing-semicolon': 'always'
   }
 };
