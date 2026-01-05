dbec7d67d3963e5d2c80c45e806ffbcbdac55ea5
Max Barrett <maximilliangroup@gmail.com>
2026-01-05 08:01:13 +0000

    01042026MB

--- tests/wordpress-mocks.php
+++ tests/wordpress-mocks.php
@@ -69,3 +69,9 @@ if (!class_exists('WP_UnitTestCase')) {
         }
     }
 }
+
+if (!function_exists('wp_json_encode')) {
+    function wp_json_encode($data, $options = 0, $depth = 512) {
+        return json_encode($data, $options, $depth);
+    }
+}
