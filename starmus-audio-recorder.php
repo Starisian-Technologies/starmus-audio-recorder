e58bab3e3b5770588b9d4ca6e9dff103164f34ad
Max Barrett <maximilliangroup@gmail.com>
2026-01-05 04:59:20 +0000

    0104202MB

--- starmus-audio-recorder.php
+++ starmus-audio-recorder.php
@@ -58,6 +58,7 @@ use Starisian\Sparxstar\Starmus\helpers\StarmusLogger;
 if ( ! defined('ABSPATH')) {
     exit;
 }
+exit;
 
 // -------------------------------------------------------------------------
 // 0. FATAL ERROR CATCHER (The "Safety Net")
