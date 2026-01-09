8e36827aec3d8fdf3d9349ce8e7fc5369526ceb4
Max Barrett <34328348+MaximillianGroup@users.noreply.github.com>
2026-01-08 22:59:46 -0800

    Add use statement for IStarmusBaseDAL interface
    Signed-off-by: Max Barrett <34328348+MaximillianGroup@users.noreply.github.com>

--- src/data/interfaces/IStarmusProsodyDAL.php
+++ src/data/interfaces/IStarmusProsodyDAL.php
@@ -11,6 +11,8 @@
 declare(strict_types=1);
 namespace Starisian\Sparxstar\Starmus\data\interfaces;
 
+use Starisian\Sparxstar\Starmus\data\interfaces\IStarmusBaseDAL;
+
 if ( ! \defined('ABSPATH')) {
     exit;
 }
